1b767862bc092a848fbfe0c85e9e0429211fadbd
FairoozAzim <37416649+FairoozAzim@users.noreply.github.com>
2021-10-31 01:02:03 +0600

    get post delete added

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const cors = require('cors');
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+//middleware 
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qzbsy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -15,18 +22,73 @@ async function run() {
       //console.log('db connected');
 
       const database = client.db('flyAwayDB');
-      const tourCollection = database.collection('tourCollection');
+      const allTours = database.collection('tourCollection');
+      const allBookings = database.collection('bookings')
       console.log('db connected');
+      //get API
+      app.get('/tourCollection', async(req,res) => {
 
-    //   app.post('/services', async(req,res) => {
-    //    const service = req.body;
-    //    console.log('data posted',service);
-       
-        //    const result = await tourCollection.insertOne(doc);
-        //    console.log(`A document was inserted with the _id: ${result.insertedId}`);
-        //    console.log(result);
-        //res.json(result);
-     // })
+         const cursor = allTours.find({});
+         const tours = await cursor.toArray();
+         res.send(tours);
+      })
+      
+      //post API to add tours
+      
+      app.post('/tourCollection', async(req,res) => {
+        const tour = req.body;
+        console.log("Post success");
+        const result = await allTours.insertOne(tour);
+        res.json(result);
+    })
+
+    
+      //single doc
+      app.get('/tourCollection/:id', async(req,res) => {
+          const id = req.params.id;
+          //console.log('Id found',id);
+          const query = {_id: ObjectId(id)};
+          const tour = await allTours.findOne(query);
+          res.json(tour);
+      })
+
+      //post API for bookings
+      app.post('/bookings', async(req,res) => {
+          const booking = req.body;
+          const result = await allBookings.insertOne(booking);
+          res.json(result);
+      })
+      
+      //get api to get all the bookings history
+      app.get('/bookings', async(req,res) => {
+        const cursor = allBookings.find({});
+        const booking = await cursor.toArray();
+        res.send(booking);
+          
+      })
+      
+      //get API to get user specific booking history
+      app.get('/bookings/:email', async(req,res) => {
+        //   const cursor = allBookings.find({email:email})
+        const email = req.params.email;
+        //console.log('email found',email);
+         const query = {email: email};
+         const bookings = await allBookings.find(query).toArray();
+         //console.log(bookings);
+         res.json(bookings);
+
+      })
+
+      //delete API to delete bookings
+     app.delete('/bookings/:id', async(req,res) => {
+         const id= req.params.id;
+         const query = {_id: ObjectId(id)};
+         const result = await allBookings.deleteOne(query);
+         console.log(result);
+         res.json(result);
+
+     })
+
     } finally {
       //await client.close();
     }
